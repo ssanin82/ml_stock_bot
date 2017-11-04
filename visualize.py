2967067d026be612800bde49219b80378af18e74
Sergiy Sanin <sanin_sergiy@yahoo.com>
2017-11-04 13:41:44 +0800

    visualization

---
+++ visualize.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from rasa_core.domain import TemplateDomain
+from rasa_core.training_utils.dsl import StoryFileReader
+from rasa_core.training_utils.visualization import visualize_stories
+
+if __name__ == '__main__':
+    domain = TemplateDomain.load("domain.yml")
+    stories = StoryFileReader.read_from_file("data/stories.md", domain)
+
+    visualize_stories(stories, "graph.png")
+
